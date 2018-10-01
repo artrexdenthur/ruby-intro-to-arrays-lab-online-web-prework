6a623ac1f2cf32be6adaadfb17649ddf654263b4
Paul Ashour <paul.ashour@gmail.com>
2018-10-01 14:51:46 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -8,5 +8,38 @@ def array_with_two_elements
   ['a', 'b']
 end
 
+def using_times
+  7.times do
+    puts 'Wingardium Leviosa'
+  end
+end
 
+def first_element(arr)
+  # takes an argument of an array and returns the first element
+  arr[0]
+end
+
+def third_element(arr)
+  # takes an arg of an array and returns the third element
+  arr[2]
+end
+
+def last_element(arr)
+  # takes an arg of an array and returns the last element
+  arr[-1]
+end
+
+def first_element_with_array_methods(arr)
+  # takes an arg of an array and returns the first element
+  # without referencing the index number
+  arr.shift
+end
+
+def last_element_with_array_methods(arr)
+  arr.pop
+end
+
+def length_of_array(arr)
+  arr.length 
+end
 
